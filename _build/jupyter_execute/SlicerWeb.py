ec697b53a53b0aed079de74b5d9822f56b965b80
aotal <aotalpalacin@gmail.com>
2020-09-30 17:21:31 +0200

    Lo que me ha generado el conversor

---
+++ _build/jupyter_execute/SlicerWeb.py
@@ -0,0 +1,19 @@
+<a href="../desktop" target="_blank"><img src="https://www.slicer.org/img/3DSlicerLogo-H-Color-218x144.png"/>
+<h1><center>Click here to open 3D Slicer in a new window</center></h1></a>
+
+# Run this cell (Shift+Enter) to show application here
+import JupyterNotebooksLib as slicernb
+slicernb.AppWindow(contents='full')
+
+# Run this cell to load a file from a URL.
+# You can load your own data by changing inputs of downloadFromURL.
+# Any questions? Write us on the Slicer forum: https://discourse.slicer.org
+slicer.mrmlScene.Clear()
+
+volume = slicernb.downloadFromURL(
+    uris="https://github.com/Slicer/SlicerTestingData/releases/download/MD5/39b01631b7b38232a220007230624c8e",
+    fileNames="MRHead.nrrd",
+    nodeNames="Volume")[0]
+
+slicernb.showVolumeRendering(volume)
+
